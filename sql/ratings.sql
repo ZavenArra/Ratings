98168357e489a344dff958e01f7a578123b0fe67
Deepwinter <deepwinter@winterroot.net>
2012-01-18 19:05:10 -0500

    Added user_id to ratings.sql

--- sql/ratings.sql
+++ sql/ratings.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `ratings` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
 	`object_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
 	`object_type_id` int(11) NOT NULL,
 	`rating` tinyint(4) NOT NULL,
 	PRIMARY KEY (`id`)
